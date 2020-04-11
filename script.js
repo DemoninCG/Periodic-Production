52de9f247886c35c697457fb4e9cdcba03b37310
DemoninCG <53471503+DemoninCG@users.noreply.github.com>
2020-04-11 18:41:28 +1200

    Update 12

--- script.js
+++ script.js
@@ -23,11 +23,27 @@ reset()
 
 function update() {
 	document.getElementById("protonAmount").innerHTML = currentNotation.format(game.protonAmount, 2, 0)
-	if (window.innerWidth >= 1340) {
+	if (window.innerWidth >= 1430) {
 		document.getElementById("periodicTable").style.left = (window.innerWidth - 1330) / 2 + "px"
+		var rowTemp = document.getElementsByClassName("rowNumber")
+		rowTemp[0].style.left = (window.innerWidth - 1400) / 2 + "px"
+		rowTemp[1].style.left = (window.innerWidth - 1400) / 2 + "px"
+		rowTemp[2].style.left = (window.innerWidth - 1400) / 2 + "px"
+		rowTemp[3].style.left = (window.innerWidth - 1400) / 2 + "px"
+		rowTemp[4].style.left = (window.innerWidth - 1400) / 2 + "px"
+		rowTemp[5].style.left = (window.innerWidth - 1400) / 2 + "px"
+		rowTemp[6].style.left = (window.innerWidth - 1400) / 2 + "px"
 	}
 	else {
-		document.getElementById("periodicTable").style.left = "5px"
+		document.getElementById("periodicTable").style.left = "50px"
+		var rowTemp = document.getElementsByClassName("rowNumber")
+		rowTemp[0].style.left = "15px"
+		rowTemp[1].style.left = "15px"
+		rowTemp[2].style.left = "15px"
+		rowTemp[3].style.left = "15px"
+		rowTemp[4].style.left = "15px"
+		rowTemp[5].style.left = "15px"
+		rowTemp[6].style.left = "15px"
 	}
 }
 
