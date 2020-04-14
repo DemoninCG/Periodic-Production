a1ebddb13d6c8596fe35d39b62845ef127d74bb1
DemoninCG <53471503+DemoninCG@users.noreply.github.com>
2020-04-15 03:54:18 +1200

    Update 16
    Completely reworked theme, changed the light/dark theme system to be more easily changeable

--- script.js
+++ script.js
@@ -23,35 +23,20 @@ reset()
 
 function update() {
 	document.getElementById("protonAmount").innerHTML = currentNotation.format(game.protonAmount, 2, 0)
-	if (window.innerWidth >= 1430) {
-		document.getElementById("periodicTable").style.left = (window.innerWidth - 1330) / 2 + "px"
-		var rowTemp = document.getElementsByClassName("rowNumber")
-		rowTemp[0].style.left = (window.innerWidth - 1400) / 2 + "px"
-		rowTemp[1].style.left = (window.innerWidth - 1400) / 2 + "px"
-		rowTemp[2].style.left = (window.innerWidth - 1400) / 2 + "px"
-		rowTemp[3].style.left = (window.innerWidth - 1400) / 2 + "px"
-		rowTemp[4].style.left = (window.innerWidth - 1400) / 2 + "px"
-		rowTemp[5].style.left = (window.innerWidth - 1400) / 2 + "px"
-		rowTemp[6].style.left = (window.innerWidth - 1400) / 2 + "px"
-	}
-	else {
-		document.getElementById("periodicTable").style.left = "50px"
-		var rowTemp = document.getElementsByClassName("rowNumber")
-		rowTemp[0].style.left = "15px"
-		rowTemp[1].style.left = "15px"
-		rowTemp[2].style.left = "15px"
-		rowTemp[3].style.left = "15px"
-		rowTemp[4].style.left = "15px"
-		rowTemp[5].style.left = "15px"
-		rowTemp[6].style.left = "15px"
-	}
 }
 
 setInterval(update, 10)
 
 
 
-
+onmousemove = function(e) {
+	if (e.clientX > window.innerWidth - 200) {
+		document.getElementById("tabBar").style.width = "200px"
+	}
+	else {
+		document.getElementById("tabBar").style.width = "40px"
+	}
+}
 
 function backPos() {
 	game.backgroundPosition += 1
@@ -59,11 +44,6 @@ function backPos() {
 }
 
 setInterval(backPos, 50)
-
-function darkMode() {
-	var element = document.body;
-	element.classList.toggle("dark-mode");
-}
 
 function protonAdd() {
 	game.protonAmount = game.protonAmount.add(1)
